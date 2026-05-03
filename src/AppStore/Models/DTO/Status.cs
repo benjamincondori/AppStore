fdeecfc13417e662d11b13755e1ab12a4eebc7f4
Benjamin Condori Vasquez <benjamin.condori@swisstech.solutions>
2026-05-03 18:48:56 -0400

    first commit

---
+++ src/AppStore/Models/DTO/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace AppStore.Models.DTO
+{
+    public class Status
+    {
+        public string? Message { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
---
+++ src/AppStore/Repositories/Abstract/ILibro.cs
@@ -0,0 +1,15 @@
+﻿using AppStore.Models.Domain;
+using AppStore.Models.DTO;
+
+namespace AppStore.Repositories.Abstract
+{
+    public interface ILibroService
+    {
+        bool Add(Libro libro);
+        bool Update(Libro libro);
+        bool Delete(int id);
+        Libro GetById(int id);
+        LibroListVm List(string term = "", bool paging = false, int currentPage = 0);
+        List<int> GetCategoriaByLibroId(int libroId);
+    }
+}
